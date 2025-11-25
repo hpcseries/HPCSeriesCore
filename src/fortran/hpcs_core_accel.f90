6dd331c939dbb95a856059bd060b44b0f7535452
Samukelo Jiyane <myjiyane@gmail.com>
2025-11-25 07:02:03 +0200

    Add CUDA support for median extraction and bitonic sort functions in hpcs_core_accel

--- src/fortran/hpcs_core_accel.f90
+++ src/fortran/hpcs_core_accel.f90
@@ -721,6 +721,7 @@ contains
   !> @param[in] sorted_data - Sorted array
   !> @param[in] n - Number of elements
   !> @return Median value
+#ifdef HPCS_USE_CUDA
   attributes(device) function gpu_extract_median(sorted_data, n) result(median_val)
     real(c_double), intent(in) :: sorted_data(:)
     integer(c_int), intent(in) :: n
@@ -734,6 +735,7 @@ contains
       median_val = (sorted_data(n/2) + sorted_data(n/2 + 1)) / 2.0_c_double
     end if
   end function gpu_extract_median
+#endif
 
   !> GPU-native small bitonic sort for window processing
   !>
@@ -743,6 +745,7 @@ contains
   !> @param[inout] window_data - Small array to sort in-place
   !> @param[in] window_size - Size of window (small)
   !> @return Median of sorted window
+#ifdef HPCS_USE_CUDA
   attributes(device) function gpu_bitonic_sort_window(window_data, window_size) result(median_val)
     real(c_double), intent(inout) :: window_data(:)
     integer(c_int), intent(in) :: window_size
@@ -780,6 +783,7 @@ contains
       median_val = (window_data(window_size/2) + window_data(window_size/2 + 1)) / 2.0_c_double
     end if
   end function gpu_bitonic_sort_window
+#endif
 
   ! ========================================================================
   ! Phase 2: HIGH PRIORITY Kernel Wrappers
